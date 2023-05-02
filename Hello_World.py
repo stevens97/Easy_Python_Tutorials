ebd72a6dfe8452442745529182e31a706b513204
stevens97 <93580712+stevens97@users.noreply.github.com>
2023-05-02 21:51:48 +0200

    Create Hello_World.py

---
+++ Hello_World.py
@@ -0,0 +1,26 @@
+# English
+print("Hello World")
+
+# Zulu
+print("Sawubona Mhlaba")
+
+# Xhosa
+print("Molo Lizwe")
+
+# Afrikaans
+print("Hallo Wêreld")
+
+# French
+print("Bonjour le monde")
+
+# German
+print("Hallo Welt")
+
+# Dutch
+print("Hallo Wereld")
+
+# Spanish
+print("Hola Mundo")
+
+# Italian
+print("Ciao Mondo")
