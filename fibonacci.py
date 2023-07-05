b4cbce7311c77db99acd3a05f9b59168dc28e22f
stevens97 <93580712+stevens97@users.noreply.github.com>
2023-07-05 09:28:32 +0200

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,27 +1,29 @@
-# Fibonacci Sequence, all numbers < 100
-# -------------------------------------------
+if __name__ == '__main__':
 
-x = [1,1]
-flag = False
-i = 2
-while flag is False:
-	temp = x[i-1]+x[i-2]
-	if temp < 100:
-		x.append(temp)
-	else:
-		flag = True
-	i=i+1
-
-print(x)
-
-# Fibonacci Sequence, first 20 numbers
-# -------------------------------------------
-
-x = [1,1]
-flag = False
-i = 2
-for i in range(20):
-	x.append(x[i-1]+x[i-2])
-
-print(x)
+	# Fibonacci Sequence, all numbers < 100
+	# -------------------------------------------
+	
+	x = [1,1]
+	flag = False
+	i = 2
+	while flag is False:
+		temp = x[i-1]+x[i-2]
+		if temp < 100:
+			x.append(temp)
+		else:
+			flag = True
+		i=i+1
+	
+	print(x)
+	
+	# Fibonacci Sequence, first 20 numbers
+	# -------------------------------------------
+	
+	x = [1,1]
+	flag = False
+	i = 2
+	for i in range(20):
+		x.append(x[i-1]+x[i-2])
+	
+	print(x)
 
